5ac475d9563ff676cf12b0f0ba2bb790ffbcea1a
Sander Ruys <ruyssander@gmail.com>
2022-10-17 16:29:57 +0200

    Start mobile responsiveness

--- scripts/blobAnim.js
+++ scripts/blobAnim.js
@@ -2,6 +2,6 @@ const tween = KUTE.fromTo(
     '#blob1',
     { path: '#blob1' },
     { path: '#blob2' },
-    {repeat: 999, duration: 3000, yoyo: true }
+    {repeat: 999, duration: 5000, yoyo: true }
 )
 tween.start();
